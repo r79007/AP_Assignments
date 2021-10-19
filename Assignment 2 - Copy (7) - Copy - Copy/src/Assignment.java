ef8776fb588356ef4e4a2ae97f26fb5f9c25a087
r79007 <83182719+r79007@users.noreply.github.com>
2021-10-19 13:34:57 +0530

    Add files via upload

---
+++ Assignment 2 - Copy (7) - Copy - Copy/src/Assignment.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+
+public class Assignment implements Assessment{
+
+    private String probStatement;
+    private int marks;
+    private String id;
+    private int assessmentID;
+    private boolean isClosed=false;
+
+    @Override
+    public boolean isClosed() {
+        return this.isClosed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.isClosed = closed;
+    }
+
+    private static HashMap<Integer,Assignment> assignmentHashMap=new HashMap<>();
+
+    public int getAssessmentID() {
+        return this.assessmentID;
+    }
+
+
+
+    public void setAssessmentID(int assessmentID) {
+        this.assessmentID = assessmentID;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
+    @Override
+    public void setProblemStatement(String s) {
+        this.probStatement=s;
+    }
+
+    public String getProblemStatement(){
+        return this.probStatement;
+    }
+
+    public void setMarks(int mark){
+        this.marks=mark;
+    }
+
+    public int getMarks() {
+        return this.marks;
+    }
+
+    public static void setAssignmentHashMap(int assessmentID,Assignment assn){
+        assignmentHashMap.put(assessmentID,assn);
+    }
+
+    public static HashMap<Integer, Assignment> getAssignmentHashMap(){
+        return assignmentHashMap;
+    }
+
+
+}
